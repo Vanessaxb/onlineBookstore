0690cfcd2611fd76a90162f2ec1a842e180cf727
Vanessa Beattie <your_git_email@email.com>
2023-09-25 11:26:40 -0500

    Order

--- controllers/api/orders.js
+++ controllers/api/orders.js
@@ -1,4 +1,4 @@
-const Order = require('../../models/order');
+const Order = require('../../models/Order');
 // const Item = require('../../models/item');
 
 
--- models/Order.js
+++ models/Order.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const itemSchema = require('./itemSchema');
+const itemSchema = require('./ItemSchema');
 
 const lineItemSchema = new Schema({
   qty: { type: Number, default: 1 },
